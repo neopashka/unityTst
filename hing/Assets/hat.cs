39b0f4685448f488f3e83d3f3585189e3e10d986
ivas <mazpah@mail.ru>
2018-07-31 00:38:27 +0300

    вторая шлямпа

---
+++ hing/Assets/hat.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hat : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void OnCollisionEnter(Collision hit){
+		if(hit.collider.name=="hat"){
+			GameObject.Find ("hat").GetComponent<Collider> ().enabled = false;
+			GameObject.Find ("hat").transform.parent = GameObject.Find ("EthanHead").transform;
+			GameObject.Find ("hat").transform.localPosition = new Vector3 (0.0f,0.0f,0.0f);
+			GameObject.Find ("hat").transform.localRotation = new Quaternion (0.0f,0.0f,0.0f,0.0f);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
